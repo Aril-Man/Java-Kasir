5ed33b0ae514c27d5a5d008d04b5a70faf03fd59
Azriel fauzi hermansyah <55310608+Azrielfhr2@users.noreply.github.com>
2020-11-11 21:35:31 +0700

    Add files via upload

---
+++ src/com/Project/Main.java
@@ -0,0 +1,24 @@
+package com.Project;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner inputuser;
+
+        float total, bayar;
+        String nama, nis;
+
+        inputuser = new Scanner(System.in);
+
+        System.out.println("==========================================");
+        System.out.println("|              Program Kasir             |");
+        System.out.println("==========================================");
+        System.out.print("Masukan nama anda : ");
+        nama = inputuser.next();
+        System.out.print("Masukan NIS : ");
+        nis = inputuser.next();
+
+    }
+}
